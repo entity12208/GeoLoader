82d05e916a4e79a0d351eef9e378e32ca8e07d52
entity12208 <116198380+entity12208@users.noreply.github.com>
2024-12-15 08:06:03 -0800

    Create install_mac.cpp

---
+++ mods/example_mod/src/mac/install_mac.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void createDirectory(const std::string& path) {
+    mkdir(path.c_str(), 0755);
+}
+
+void copyFile(const std::string& src, const std::string& dest) {
+    std::ifstream srcFile(src, std::ios::binary);
+    std::ofstream destFile(dest, std::ios::binary);
+    destFile << srcFile.rdbuf();
+}
+
+void install() {
+    std::string installPath = "/usr/local/geoloader";
+    std::string binaryPath = "binaries/geometry_dash_mac";
+
+    createDirectory(installPath);
+    copyFile(binaryPath, installPath + "/geometry_dash");
+    chmod((installPath + "/geometry_dash").c_str(), 0755);
+
+    std::cout << "Installation successful on macOS!" << std::endl;
+}
+
+int main() {
+    install();
+    return 0;
+}
